1c20ab88c6f2d44cba53c73fe38a04b0b32f1fba
Sergei Dolgov <dolgovs@gmail.com>
2012-12-09 23:35:10 +0100

    Add coursera-download.sh

---
+++ coursera-download.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# download coursera videos
+
+if [ ! $# -eq 2 ]; then
+    echo "Usage: $0 <cookie header> <index page url>"
+    exit 1
+fi
+
+COOKIE_HEADER="$1"
+INDEX_URL="$2"
+
+EXTRACT_PATTERN=${EXTRACT_PATTERN-.mp4}
+
+shift 2
+
+_wget () {
+    wget -c --content-disposition --header "$COOKIE_HEADER" "$@"
+}
+
+dl_index () {
+    _wget -O - "$INDEX_URL"
+}
+
+extract_urls () {
+    grep "$EXTRACT_PATTERN" | while read LINE; do
+        URL=${LINE#*href=\"}
+        echo ${URL%\"}
+    done
+}
+
+dl_files () {
+    while read URL; do
+        _wget $URL
+    done
+}
+
+dl_index | extract_urls | dl_files
